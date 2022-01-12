9d69f8f391d1518067884f7ce9ed413de1a7dd45
Rodolfo Tagle <rodolfo@torre.co>
2022-01-11 23:30:58 -0600

    Add Toolbar

---
+++ components/toolbar/Toolbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex justify-between items-center h-12 px-4 shadow-md text-sm">
+    <div>
+      <Transition name="slide">
+        <slot name="left">
+          <div key="left">
+            <span class="font-bold text-blue-700">Blog</span>
+          </div>
+        </slot>
+      </Transition>
+    </div>
+    <div>
+      <Transition name="slide" mode="out-in">
+        <slot name="center">
+          <div class="text-red-600">No slot content</div>
+        </slot>
+      </Transition>
+    </div>
+    <div>
+      <Transition name="slide" mode="out-in">
+        <slot name="right">
+          <div class="text-red-600">No slot content</div>
+        </slot>
+      </Transition>
+    </div>
+  </div>
+</template>
+
+<script src="./Toolbar.scss" lang="scss" scoped />
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <toolbar />
+    <Nuxt />
+  </div>
+</template>
