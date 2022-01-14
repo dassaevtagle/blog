c6ce027a405d45a5ff4e94cc90c46a3fa60c8d1f
Rodolfo Tagle <rodolfo@torre.co>
2022-01-14 01:26:35 -0600

    Add color palette main colors
    https://coolors.co/3c91e6-342e37-a2d729-fafffd-fa824c

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+      extend: {
+          colors: {
+            "b-green": '#A2D729',
+            "b-dark": '#342E37',
+            "b-light": '#FAFFFD',
+            "b-orange": '#FA824C',
+            "b-blue": "#3C91E6"
+          }
+      }
+  },
+}
